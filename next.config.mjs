966e3cc4321ab5e404193ba9dda9f015f27ccc6d
Micheal <khanzaidan786@gmail.com>
2025-01-04 14:26:20 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
+  trailingSlash: true,
+  swcMinify: true,
+  basePath: "",
+  assetPrefix: "",
   images: {
+    dangerouslyAllowSVG: true,
     remotePatterns: [
       {
         protocol: "https",
