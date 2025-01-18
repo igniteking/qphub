24bf4a438e4cfa6e5d5db9f698f25888c11da636
Micheal <khanzaidan786@gmail.com>
2025-01-18 15:15:23 +0530

    changes
    added a fallback screen

--- app/(app)/(main)/(jobs)/candidate-list/CandidatesTable.tsx
+++ app/(app)/(main)/(jobs)/candidate-list/CandidatesTable.tsx
@@ -1,8 +1,8 @@
 "use client";
+
 import { Joblistdata } from "@/shared/data/dashboards/joblistdata";
 import Link from "next/link";
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   CandidateData,
   Education,
@@ -12,6 +12,10 @@ import {
   Skill,
   Technology,
 } from "@/shared/types/types";
+import Loader from "@/components/Loader";
+import ToastNotification from "@/components/ToastNotification";
+import CodeBroke from "@/components/CodeBroke";
+
 interface ResumeData {
   candidateData: CandidateData;
   education: Education[];
@@ -21,10 +25,13 @@ interface ResumeData {
   skills: Skill[];
   technologies: Technology[];
 }
+
 const CandidatesTable = () => {
   const [resumeData, setResumeData] = useState<ResumeData | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [message, setMessage] = useState<string>("");
+  const [showMessage, setShowMessage] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,6 +44,8 @@ const CandidatesTable = () => {
         setResumeData(data);
       } catch (err: any) {
         setError(err.message);
+        setMessage(err.message);
+        setShowMessage(true);
       } finally {
         setLoading(false);
       }
@@ -45,10 +54,17 @@ const CandidatesTable = () => {
     fetchData();
   }, []);
 
+  if (error) return <CodeBroke />;
+
   return (
     <div className="table-responsive">
       <table className="table table-hover text-nowrap">
         <thead>
+          <ToastNotification
+            show={showMessage}
+            setShow={setShowMessage}
+            message={message}
+          />
           <tr>
             <th scope="row" className="ps-4">
               <input
@@ -72,81 +88,92 @@ const CandidatesTable = () => {
           </tr>
         </thead>
         <tbody>
-          {Joblistdata.map((idx) => (
-            <tr key={Math.random()}>
-              <td className="ps-4">{idx.checked}</td>
-              <td>
-                <div className="d-flex">
-                  <span className="avatar avatar-md avatar-rounded bg-primary bg-opacity-10">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      data-name="Layer 1"
-                      viewBox="0 0 24 24"
-                    >
-                      <path fill="var(--primary-color)" d={idx.class2}></path>
-                    </svg>
-                  </span>
-                  <div className="ms-2">
-                    <p className="fw-medium mb-0 d-flex align-items-center">
-                      <a href="/apps/jobs/jobdetails/">{idx.class}</a>
-                    </p>
-                    <p className="fs-12 text-muted mb-0">{idx.class1}</p>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <div className="d-flex align-items-center">
-                  <span className="avatar avatar-sm p-1 me-1 bg-light avatar-rounded">
-                    <img src={idx.src} alt="" />
-                  </span>
-                  <Link scroll={false} href="#!" className="fw-medium mb-0">
-                    {idx.text1}
-                  </Link>
-                </div>
-              </td>
-              <td>{idx.text3}</td>
-              <td>{idx.number}</td>
-              <td>{idx.number1}</td>
-              <td>
-                <span
-                  className={`badge rounded-pill bg-${idx.color1}-transparent`}
-                >
-                  {idx.text}
-                </span>
-              </td>
-              <td>{idx.data}</td>
-              <td>{idx.data1}</td>
-              <td>
-                <span className={`badge bg-${idx.color1}-transparent`}>
-                  <i className="bi bi-clock me-1"></i>
-                  {idx.text2}
-                </span>
-              </td>
-              <td>
-                <Link
-                  scroll={false}
-                  href="/apps/jobs/jobdetails/"
-                  className="btn btn-icon btn-sm btn-primary-light btn-wave waves-effect waves-light me-1"
-                >
-                  <i className="ri-eye-line"></i>
-                </Link>
-                <Link
-                  scroll={false}
-                  href="#!"
-                  className="btn btn-icon btn-sm btn-info-light btn-wave waves-effect waves-light me-1"
-                >
-                  <i className="ri-edit-line"></i>
-                </Link>
-                <Link
-                  scroll={false}
-                  href="#!"
-                  className="btn btn-icon btn-sm btn-danger-light btn-wave waves-effect waves-light"
-                >
-                  <i className="ri-delete-bin-line"></i>
-                </Link>
+          {loading ? (
+            <tr>
+              <td colSpan={11}>
+                <Loader />
               </td>
             </tr>
-          ))}
+          ) : (
+            Joblistdata.map((item, idx) => (
+              <tr key={idx}>
+                <td className="ps-4">{item.checked}</td>
+                <td>
+                  <div className="d-flex">
+                    <span className="avatar avatar-md avatar-rounded bg-primary bg-opacity-10">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        data-name="Layer 1"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="var(--primary-color)"
+                          d={item.class2}
+                        ></path>
+                      </svg>
+                    </span>
+                    <div className="ms-2">
+                      <p className="fw-medium mb-0 d-flex align-items-center">
+                        <a href="/apps/jobs/jobdetails/">{item.class}</a>
+                      </p>
+                      <p className="fs-12 text-muted mb-0">{item.class1}</p>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <div className="d-flex align-items-center">
+                    <span className="avatar avatar-sm p-1 me-1 bg-light avatar-rounded">
+                      <img src={item.src} alt="" />
+                    </span>
+                    <Link scroll={false} href="#!" className="fw-medium mb-0">
+                      {item.text1}
+                    </Link>
+                  </div>
+                </td>
+                <td>{item.text3}</td>
+                <td>{item.number}</td>
+                <td>{item.number1}</td>
+                <td>
+                  <span
+                    className={`badge rounded-pill bg-${item.color1}-transparent`}
+                  >
+                    {item.text}
+                  </span>
+                </td>
+                <td>{item.data}</td>
+                <td>{item.data1}</td>
+                <td>
+                  <span className={`badge bg-${item.color1}-transparent`}>
+                    <i className="bi bi-clock me-1"></i>
+                    {item.text2}
+                  </span>
+                </td>
+                <td>
+                  <Link
+                    scroll={false}
+                    href="/apps/jobs/jobdetails/"
+                    className="btn btn-icon btn-sm btn-primary-light btn-wave waves-effect waves-light me-1"
+                  >
+                    <i className="ri-eye-line"></i>
+                  </Link>
+                  <Link
+                    scroll={false}
+                    href="#!"
+                    className="btn btn-icon btn-sm btn-info-light btn-wave waves-effect waves-light me-1"
+                  >
+                    <i className="ri-edit-line"></i>
+                  </Link>
+                  <Link
+                    scroll={false}
+                    href="#!"
+                    className="btn btn-icon btn-sm btn-danger-light btn-wave waves-effect waves-light"
+                  >
+                    <i className="ri-delete-bin-line"></i>
+                  </Link>
+                </td>
+              </tr>
+            ))
+          )}
         </tbody>
       </table>
     </div>
