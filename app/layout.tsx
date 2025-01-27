b1991be2898498bc4a13a1247f2a54f50fbae4e5
Micheal <khanzaidan786@gmail.com>
2025-01-27 15:16:32 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import { Provider } from "react-redux";
 import "./globals.scss";
 import store from "@/shared/redux/store";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react"
 
 const RootLayout = ({ children }: any) => {
   return (
     <>
+      <Analytics />
       <Provider store={store}>
         <ClerkProvider>{children}</ClerkProvider>
       </Provider>
