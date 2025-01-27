cfd7a2a7524159a61cd6a2359dc1d98da0073890
Micheal <khanzaidan786@gmail.com>
2025-01-27 15:20:50 +0530

    Speed Insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,14 @@ import { Provider } from "react-redux";
 import "./globals.scss";
 import store from "@/shared/redux/store";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const RootLayout = ({ children }: any) => {
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       <Provider store={store}>
         <ClerkProvider>{children}</ClerkProvider>
       </Provider>
